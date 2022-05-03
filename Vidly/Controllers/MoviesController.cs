8b48cb11bf00e16e533d26f885183b02d83670e2
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-05-04 01:34:32 +0530

    Add project files.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new Movie() { Id = 1, Title="DDLJ" };
+            return View(movies);
+        }
+    }
+}
+
+//@Scripts.Render("~/bundles/bootstrap")
