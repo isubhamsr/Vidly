512c2717877d3f3e0283d5f6cd405fb7092ff77c
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2022-05-05 01:15:07 +0530

    Add customer view

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name ="Subham"},
+                new Customer { Id = 2, Name ="Sohini"},
+                new Customer { Id = 3, Name ="Avantika"},
+            };
+
+            var customerView = new CustomerViewModel
+            {
+                Customets = customers,
+            };
+            return View(customerView);
+        }
+
+        public ActionResult Details(int id)
+        {
+           
+            if (id == 1)
+            {
+                return Content("Subham");
+            }
+            else if (id == 2)
+            {
+                return Content("Sohini");
+            }else
+            {
+                return Content("Avantika");
+            }
+            
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModel;
 
 namespace Vidly.Controllers
 {
@@ -12,8 +13,18 @@ namespace Vidly.Controllers
         // GET: Movies
         public ActionResult Index()
         {
-            var movies = new Movie() { Id = 1, Title="DDLJ" };
-            return View(movies);
+            var movies = new List<Movie>
+            {
+                new Movie { Id=1, Title = "DDLJ"},
+                new Movie { Id=2, Title = "Dangal"},
+                new Movie { Id=3, Title = "PK"}
+            };
+
+            var movieView = new MoviesViewModel
+            {
+                Movies = movies
+            };
+            return View(movieView);
         }
     }
 }
